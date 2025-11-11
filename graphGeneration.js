4b14f830b5810eeb169b38222e031a4e054b73fa
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2025-11-11 10:46:30 +0530

    Updates

--- graphGeneration.js
+++ graphGeneration.js
@@ -96,6 +96,11 @@ export function generateRandomGraph() {
     elements.svg.appendChild(node.circle);
     elements.svg.appendChild(node.text);
   });
+
+  console.log("Nodes:", state.nodes);
+  console.log("Edges:", state.edges);
+
+
 }
 
 export function resetGraph() {
